7dc7057bfeee3080913478e88e0761e833260fe4
Pawel <rokosz.pawel@gmail.com>
2018-02-28 21:27:22 +0100

    onChange handler added

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ type, action, placeholder}) => (
-  <input type={action} className={type} placeholder={placeholder}/>
+const Input = ({ type, action, placeholder, onChange}) => (
+  <input type={action} className={type} placeholder={placeholder} onChange={onChange}/>
 );
 
 Text.propTypes = {
   type: PropTypes.oneOf(['input', 'input--positive', 'input--negative']),
   placeholder: PropTypes.string,
   action: PropTypes.oneOf(['submit', 'text', 'checkbox', 'radio']),
+  onChange: PropTypes.func
 };
 
 Text.defaultProps = {
-  action: 'text'
+  action: 'text',
+  onChange: () => {}
 };
 
 export default Input;
