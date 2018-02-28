c697a0ce821700f5c561365367bea60b53428fb3
Pawel <rokosz.pawel@gmail.com>
2018-02-28 21:30:33 +0100

    value added to input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ type, action, placeholder, onChange}) => (
-  <input type={action} className={type} placeholder={placeholder} onChange={onChange}/>
+const Input = ({ type, action, placeholder, onChange, value}) => (
+  <input type={action} className={type} placeholder={placeholder} onChange={onChange} value={value}/>
 );
 
 Text.propTypes = {
   type: PropTypes.oneOf(['input', 'input--positive', 'input--negative']),
   placeholder: PropTypes.string,
   action: PropTypes.oneOf(['submit', 'text', 'checkbox', 'radio']),
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  value: PropTypes.string
 };
 
 Text.defaultProps = {
   action: 'text',
-  onChange: () => {}
+  onChange: () => {},
+  value: '',
+  placeholder: ''
 };
 
 export default Input;
